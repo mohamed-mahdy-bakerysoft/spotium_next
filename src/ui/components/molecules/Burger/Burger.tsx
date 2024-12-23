e4a91c55d76e6aa867417854faa919b1e76d318c
MA1zaxaka1996 <alexracovets@gmail.com>
2024-12-23 02:58:55 +0200

    fixed style

--- src/ui/components/molecules/Burger/Burger.tsx
+++ src/ui/components/molecules/Burger/Burger.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { cn } from "@/src/utils/cn";
 import { useState } from "react";
 
-const transitions = "transition-all duration-300 ease-in-out before:transition-all before:duration-300 before:ease-in-out after:transition-all after:duration-300 after:ease-in-out";
-const before = "before:content-[''] before:absolute before:left-[30%] before:top-[50%] before:w-[1rem] before:h-[1rem] before:translate-x-[-50%] before:translate-y-[-50%] before:bg-regalBlack before:rounded-[50%]";
-const after = "after:content-[''] after:absolute after:right-[30%] after:top-[50%] after:w-[1rem] after:h-[1rem] after:translate-x-[50%] after:translate-y-[-50%] after:bg-regalBlack after:rounded-[50%]";
-const hoverBefore = "before:top-[30%] before:left-[50%] before:w-[2.719rem] before:h-[2.719rem]";
-const hoverAfter = "after:top-[80%] after:right-[50%]";
+import { BurgerButton } from "@/src/ui/components/atoms/BurgerButton";
+import { cn } from "@/src/utils/cn";
 
 export const Burger = () => {
     const [isHovered, setIsHovered] = useState(false);
@@ -16,22 +12,14 @@ export const Burger = () => {
         <div
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-            className="flex justify-center items-center cursor-pointer gap-x-[1.5rem]"
+            className="flex justify-center items-center gap-x-[1.5rem] cursor-pointer"
         >
-            <div className="uppercase text-[4.8rem] font-bebas tracking-wide">
+            <div className={cn(
+                "uppercase text-[4.8rem] font-bebas tracking-wide transition-opacity duration-300 ease-in-out select-none"
+            )}>
                 Menu
             </div>
-            <div className="w-[6.359rem] h-[6.359rem] flex justify-center items-center">
-                <div className={cn(
-                    "w-full h-[4.28rem] bg-regalWhite rounded-[3rem] relative",
-                    transitions,
-                    before,
-                    after,
-                    isHovered && "w-[4.28rem] h-full",
-                    isHovered && hoverBefore,
-                    isHovered && hoverAfter
-                )} />
-            </div>
+            <BurgerButton isHovered={isHovered} />
         </div>
     );
 };
